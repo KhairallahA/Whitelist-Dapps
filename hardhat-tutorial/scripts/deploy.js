16aec1240a22fbe8120627fa9b4f72be6027c24c
KhairallahA <khairallahfreelancer@gmail.com>
2022-09-19 15:14:58 +0300

    Deploy Project

---
+++ hardhat-tutorial/scripts/deploy.js
@@ -0,0 +1,17 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const whitelistContract = await ethers.getContractFactory("Whitelist")
+
+  const deployedWhitelistContract = await whitelistContract.deploy(10)
+
+  await deployedWhitelistContract.deployed()
+
+  console.log("Whitelist Contract Address:", deployedWhitelistContract.address)
+}
+
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
